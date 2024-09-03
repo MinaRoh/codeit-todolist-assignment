72a42c1c7babf6f2824bf1337bb40b9424bc7934
MinaRoh <00minha@gmail.com>
2024-09-03 19:59:55 +0900

    Revert "💄style: colors 추가"
    This reverts commit bebbd1744303118d893c1e67e0e56d308d56bf6e.

--- src/style/colors.ts
+++
@@ -1,9 +0,0 @@
-export const colors = {
-  slate_200: 'bg-slate-200',
-  slate_900: 'bg-slate-900',
-  violet: 'bg-violet-500',
-  rose: 'bg-rose-500',
-  lime: 'bg-lime-300',
-  amber: 'bg-amber-800',
-  orange: 'bg-orange-500',
-}
