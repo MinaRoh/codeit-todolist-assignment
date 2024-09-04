8f0586188525ad83a327eab7b08b95919ace92df
MinaRoh <00minha@gmail.com>
2024-09-04 15:09:57 +0900

    ✨ Feat: TodoInput 공용 컴포넌트 추가
    -components/common/TodoInput.tsx에 Button 공용컴포넌트를 추가했습니다.
    - styles/inputs.css에 TodoInput 컴포넌트 사용시 재사용되는 tailwind css class들을 @apply를 이용해 추가했습니다.

---
+++ src/app/components/common/TodoInput.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState } from 'react';
+
+interface InputProps {
+  text: string;
+  detailPage?: boolean;
+  additionalClasses?: string; // 추가된다면 line-through
+}
+            
+const TodoInput = ({ text, detailPage, additionalClasses = '' }: InputProps) => {
+  const [inputValue, setInputValue] = useState(text); // 기존에 입력되어있던 text를 기본값으로
+
+  const onTodoInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+  const todoInputClasses = [
+    'input-base',
+    detailPage && 'input-detail',
+    additionalClasses,
+    ].join(' ');
+
+  return (
+    <input type='text' value={inputValue} className={todoInputClasses} onChange={onTodoInputChange}/>
+  );
+};
+
+export default TodoInput;
