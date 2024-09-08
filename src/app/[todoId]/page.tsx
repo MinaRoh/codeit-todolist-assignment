73293380aecbb2b513d1a76e263f48e990311476
MinaRoh <00minha@gmail.com>
2024-09-08 22:01:27 +0900

    🔇 Chore: 불필요한 console.log 제거

--- src/app/[todoId]/page.tsx
+++ src/app/[todoId]/page.tsx
@@ -6,7 +6,6 @@ interface params{
   }
 }
 const TodoDetailPage = ({ params: { todoId } }: params) => {
-  console.log(todoId);
   return ( 
     <main className='flex min-h-screen min-w-screen flex-col items-center p-4 gap-6 desktop:px-28 desktop-fhd:px-[360px] desktop:mx-36 desktop-fhd:max-h-80 desktop:bg-white desktop-fhd:bg-white'>
       <TodoDetail todoId={Number(todoId)} />
